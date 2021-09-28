d083e71af608e5845064111a91f0d1539bdbd26d
Othniel Chan <othnielhr.chan@gmail.com>
2021-09-27 22:53:50 -0500

    working on actions

--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,10 @@
-var changeVideoList = (videos) => ({
+var changeVideoList = (videos) => {
   //TODO:  Return some action object to change the list of videos in VideoList.
-});
+  //return object with type and payload
+  return {
+    type: 'changeVideoList',
+    payload: videos
+  };
+};
 
 export default changeVideoList;
